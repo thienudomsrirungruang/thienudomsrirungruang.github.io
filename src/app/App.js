7e56722e56f350d4cf5ee35ec2c94c2a63f51908
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2021-02-03 18:05:24 +0700

    Added basic project

---
+++ src/app/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import IconButton from './IconButton';
+
+function App() {
+    return (
+        <div id="window_container">
+            <div id="top"></div>
+            <div id="bottom">
+                <div class="centerer">
+                    <IconButton></IconButton>
+                </div>
+            </div>
+            <div class="middle_container">
+                <div class="picture"></div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/app/IconButton.js
@@ -0,0 +1,11 @@
+import './IconButton.css';
+
+function IconButton() {
+    return (
+        <div>
+            button
+        </div>
+    );
+}
+
+export default IconButton;
