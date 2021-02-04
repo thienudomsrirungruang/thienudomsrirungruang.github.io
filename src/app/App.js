eab4e61cdfa35b676a77257f83c92831462ba89b
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2021-02-04 08:44:01 +0700

    Add material design + github icon

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import IconButton from './IconButton';
+import SocialIcon from './SocialIcon';
+import Box from '@material-ui/core/Box';
+import GithubIcon from '../assets/icons/GithubIcon.svg';
 
 function App() {
     return (
         <div id="window_container">
-            <div id="top"></div>
+            <Box boxShadow={5} id="top">
+
+            </Box>
             <div id="bottom">
                 <div class="centerer">
-                    <IconButton></IconButton>
+                    <SocialIcon
+                        link="https://github.com/thienudomsrirungruang"
+                        icon={GithubIcon}
+                        aria_label="github"
+                        alt="Github"
+                    />
                 </div>
             </div>
             <div class="middle_container">
---
+++ src/app/SocialIcon.js
@@ -0,0 +1,14 @@
+import './SocialIcon.css';
+import { Divider, IconButton, SvgIcon, Tooltip } from '@material-ui/core';
+
+function SocialIcon(props) {
+    return (
+        <div class="grayout">
+            <a aria-label={props.aria_label} href={props.link}>
+                <img src={props.icon} alt={props.alt} class="btn"></img>
+            </a>
+        </div>
+    );
+}
+
+export default SocialIcon;
