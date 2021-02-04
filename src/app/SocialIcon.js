bbe05bab1443be7a214430d5a2a7dac13fd29952
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2021-02-04 09:34:37 +0700

    Add more icons

--- src/app/SocialIcon.js
+++ src/app/SocialIcon.js
@@ -3,7 +3,7 @@ import { Divider, IconButton, SvgIcon, Tooltip } from '@material-ui/core';
 
 function SocialIcon(props) {
     return (
-        <div class="grayout">
+        <div class="grayout icon_wrapper">
             <a aria-label={props.aria_label} href={props.link}>
                 <img src={props.icon} alt={props.alt} class="btn"></img>
             </a>
