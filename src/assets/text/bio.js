133fa028be1dff4e69c055d93d472390062c3a00
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2021-02-05 14:01:50 +0700

    Removed unnecessary newline

--- src/assets/text/bio.js
+++ src/assets/text/bio.js
@@ -1,6 +1,6 @@
 export default `Hi! I'm a high school student (currently in my final year) who's interested in computer science, programming and math. I've been programming since I was 9, and have done some interesting and fun projects since then - have a look below!
-\n
+
 Some of my more specific academic interests are in artificial intelligence and theoretical computer science, and I've taken part in multiple online programming competitions, getting a Top 200 finish in the Facebook Hacker Cup 2020.
-\n
+
 I'm also interested in speedcubing and rhythm games.`;
 
