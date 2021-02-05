b0a33580327064db1a0221adbc0a1d255dae333e
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2021-02-05 14:04:54 +0700

    Fix warning that bio was anonymous export

--- src/assets/text/bio.js
+++ src/assets/text/bio.js
@@ -1,6 +1,7 @@
-export default `Hi! I'm a high school student (currently in my final year) who's interested in computer science, programming and math. I've been programming since I was 9, and have done some interesting and fun projects since then - have a look below!
+const bio = `Hi! I'm a high school student (currently in my final year) who's interested in computer science, programming and math. I've been programming since I was 9, and have done some interesting and fun projects since then - have a look below!
 
 Some of my more specific academic interests are in artificial intelligence and theoretical computer science, and I've taken part in multiple online programming competitions, getting a Top 200 finish in the Facebook Hacker Cup 2020.
 
 I'm also interested in speedcubing and rhythm games.`;
 
+export default bio;
