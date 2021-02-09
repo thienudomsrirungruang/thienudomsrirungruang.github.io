b55e7b8bc3f85a168e4b42b25ce80f80b13f58d5
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2021-02-09 15:58:53 +0700

    Changed impostor text

--- src/assets/text/Impostor.js
+++ src/assets/text/Impostor.js
@@ -1,3 +1,3 @@
-const impostor = `A chatbot that uses AI to emulate my texting habits. Uses GPT-2 trained on 20,000 of my chat messages. Can be interacted with using the messaging app Discord.`;
+const impostor = `A chatbot that uses AI to emulate my texting habits. Uses the GPT-2 language model trained on 20,000 of my chat messages. Can be interacted with using the messaging app Discord (the bot is not currently hosted, but you can train and host the bot yourself).`;
 
 export default impostor;
