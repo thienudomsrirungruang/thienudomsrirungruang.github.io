054d10ba89c9470260ae53c086bd122f50ab3beb
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2021-02-09 11:45:18 +0700

    Add projects section

--- src/app/MainContent.js
+++ src/app/MainContent.js
@@ -1,6 +1,7 @@
 import "./MainContent.css";
 import { Container, Grid } from "@material-ui/core";
-import bio from "../assets/text/bio.js"
+import bio from "../assets/text/Bio.js"
+import timetable from "../assets/text/TimetableSpreadsheet.js"
 import FadeInSection from "./FadeInSection";
 import PlaceholderImage from "../assets/images/placeholder.png"
 import Paragraphize from "./Paragraphize";
@@ -9,14 +10,34 @@ function MainContent(props){
     return (
         <div className="content_wrapper">
             <Container>
+                <h1>Bio</h1>
                 <Grid container spacing={5}>
-                    <Grid item xs={12} sm={6}>
+                    <Grid item xs={12} md={6}>
                         <FadeInSection fade_direction="fade_right">
-                            <h1>Bio</h1>
                             <Paragraphize content={bio}></Paragraphize>
                         </FadeInSection>
                     </Grid>
-                    <Grid item xs={12} sm={6}>
+                    <Grid item xs={12} md={6}>
+                        <FadeInSection fade_direction="fade_left">
+                            <img class="inline_image" src={PlaceholderImage} alt="placeholder"/>
+                        </FadeInSection>
+                    </Grid>
+                </Grid>
+                <h1>Projects</h1>
+                <Grid container spacing={5}>
+                    <Grid item xs={12} md={8}>
+                        <FadeInSection fade_direction="fade_right">
+                            <h3>Timetable Spreadsheet</h3>
+                            <Paragraphize content={timetable}></Paragraphize>
+                            <p>You can find this <a href="https://docs.google.com/spreadsheets/d/1whLY_6DZZo7oUEzyGk94aX59XgR2EmK9iYAlogeQ8kE/edit?usp=sharing">
+                                here
+                            </a> and the instructions <a href="https://docs.google.com/document/d/1JAS3doUkGZXRaHi1lSiXcSqMTLsJ4HO2N7qkAgVb070/edit?usp=sharing">
+                                here
+                            </a>.
+                            </p>
+                        </FadeInSection>
+                    </Grid>
+                    <Grid item xs={12} md={4}>
                         <FadeInSection fade_direction="fade_left">
                             <img class="inline_image" src={PlaceholderImage} alt="placeholder"/>
                         </FadeInSection>
---
+++ src/assets/text/TimetableSpreadsheet.js
@@ -0,0 +1,5 @@
+const timetable = `A google sheet that generates and exports a school csv timetable to import into your favourite online calendars. This was made with my school's 10-day cycle in mind, so it might not work with most schedules.
+
+This was my second major project in Google Sheets, and I made heavy use of Google Apps Script to automatically generate the tables and work with user input in the sheets.`;
+
+export default timetable;
