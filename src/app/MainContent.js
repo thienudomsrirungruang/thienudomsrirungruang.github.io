68f34942f2ad277cecaa7b0ab81f004555efba1e
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2021-02-09 15:41:53 +0700

    Changed location of FadeInSections + add impostor image

--- src/app/MainContent.js
+++ src/app/MainContent.js
@@ -6,28 +6,27 @@ import impostor from "../assets/text/Impostor.js"
 import FadeInSection from "./FadeInSection";
 import PlaceholderImage from "../assets/images/placeholder.png"
 import Paragraphize from "./Paragraphize";
+import ImpostorImage from "../assets/images/impostor.png"
 
 function MainContent(props){
     return (
         <div className="content_wrapper">
             <Container>
-                <h1>Bio</h1>
-                <Grid container spacing={5}>
-                    <Grid item xs={12} md={6}>
-                        <FadeInSection fade_direction="fade_right">
+                <FadeInSection fade_direction="fade_right">
+                    <h1>Bio</h1>
+                    <Grid container spacing={5}>
+                        <Grid item xs={12} md={6}>
                             <Paragraphize content={bio}></Paragraphize>
-                        </FadeInSection>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <FadeInSection fade_direction="fade_left">
+                        </Grid>
+                        <Grid item xs={12} md={6}>
                             <img className="inline_image" src={PlaceholderImage} alt="placeholder"/>
-                        </FadeInSection>
+                        </Grid>
                     </Grid>
-                </Grid>
-                <h1>Projects</h1>
-                <Grid container spacing={5}>
-                    <Grid item xs={12} md={8}>
-                        <FadeInSection fade_direction="fade_right">
+                </FadeInSection>
+                <FadeInSection fade_direction="fade_right">
+                    <h1>Projects</h1>
+                    <Grid container spacing={5}>
+                        <Grid item xs={12} md={8}>
                             <h3>Timetable Spreadsheet</h3>
                             <Paragraphize content={timetable}></Paragraphize>
                             <p>You can find this <a href="https://docs.google.com/spreadsheets/d/1whLY_6DZZo7oUEzyGk94aX59XgR2EmK9iYAlogeQ8kE/edit?usp=sharing">
@@ -36,25 +35,19 @@ function MainContent(props){
                                 here
                             </a>.
                             </p>
-                        </FadeInSection>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <FadeInSection fade_direction="fade_left">
+                        </Grid>
+                        <Grid item xs={12} md={4}>
                             <img className="inline_image" src={PlaceholderImage} alt="placeholder"/>
-                        </FadeInSection>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <FadeInSection fade_direction="fade_right">
-                            <img className="inline_image" src={PlaceholderImage} alt="placeholder"/>
-                        </FadeInSection>
-                    </Grid>
-                    <Grid item xs={12} md={8}>
-                        <FadeInSection fade_direction="fade_left">
+                        </Grid>
+                        <Grid item xs={12} md={3}>
+                            <img className="inline_image" src={ImpostorImage} alt="placeholder"/>
+                        </Grid>
+                        <Grid item xs={12} md={9}>
                             <h3>Impostor Chatbot</h3>
                             <Paragraphize content={impostor}></Paragraphize>
-                        </FadeInSection>
+                        </Grid>
                     </Grid>
-                </Grid>
+                </FadeInSection>
             </Container>
         </div>
     );
